2c563bdcc28792d0bbda5bffdcfa7d05fea75e81
alin00r <alinoorspam@gmail.com>
2024-03-23 07:51:03 +0200

    enable CORS and Compress all responses

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const path = require("path");
-
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cors = require("cors");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const compression = require("compression");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
@@ -18,34 +21,41 @@ dbConnection();
 // express app
 const app = express();
 
+// Enable CROS
+app.use(cors());
+app.options("*", cors());
+
+// compress all responses
+app.use(compression());
+
 // Middlewares
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "uploads")));
 
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
-    console.log(`mode ${process.env.NODE_ENV}`);
+  app.use(morgan("dev"));
+  console.log(`mode ${process.env.NODE_ENV}`);
 }
 
 // Mount Routes
 mountRoutes(app);
 
 app.all("*", (req, res, next) => {
-    next(new ApiError(`Can't find this route: ${req.originalUrl}`, 404));
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 404));
 });
 // Global error handling middleware For express
 app.use(globalError);
 
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`);
+  console.log(`App running on port ${PORT}`);
 });
 
 // Handle rejection outside express
 process.on("unhandledRejection", (err) => {
-    console.error(`UnhandledRegjection Errors: ${err.name}| ${err.message}`);
-    server.close(() => {
-        console.error("Shutting down....");
-        process.exit(1);
-    });
+  console.error(`UnhandledRegjection Errors: ${err.name}| ${err.message}`);
+  server.close(() => {
+    console.error("Shutting down....");
+    process.exit(1);
+  });
 });
